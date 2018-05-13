8d57310f26aaac1f0626d44693e0c4181489c823
Oksana Tkachenko <oksana@crystax.net>
2018-05-13 14:36:16 +0300

    WIP, added jquery and button hamburger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.sass';
+console.log('webpack');
+
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#hamburger').on('click', function(e) {
+    e.preventDefault();
+    $('#navigation').toggleClass('header-nav__menu__mobile-open');
+  });
+});
