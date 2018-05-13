220927f5a93a3fc0457b64b0a5f634d9d8ad70c9
Oksana Tkachenko <oksana@crystax.net>
2018-05-13 21:23:11 +0300

    WIP, added button for close(mobile open)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 $(document).ready(function() {
   $('#hamburger').on('click', function(e) {
     e.preventDefault();
+    $(this).toggleClass('hamburger__active');
     $('#navigation').toggleClass('header__mobile-open');
   });
 });
