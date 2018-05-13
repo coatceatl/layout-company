a03cc114df2089630a75ab04b443352c70bca090
Oksana Tkachenko <oksana@crystax.net>
2018-05-13 17:47:14 +0300

    WIP, changed style for mobile-open

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './sass/main.sass';
-console.log('webpack');
-
 import $ from 'jquery';
 
 $(document).ready(function() {
   $('#hamburger').on('click', function(e) {
     e.preventDefault();
-    $('#navigation').toggleClass('header-nav__menu__mobile-open');
+    $('#navigation').toggleClass('header__mobile-open');
   });
 });
